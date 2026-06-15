586c1e52ba89c4c745c11b284459a33261098b40
hmd1233 <hamadhumaid050@gmail.com>
2026-06-15 11:05:16 -0700

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from .models import (
+    Course,
+    Lesson,
+    Instructor,
+    Learner,
+    Question,
+    Choice,
+    Submission
+)
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('content', 'grade')
+
+
+class LessonAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+    list_display = ('title',)
+
+
+admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
