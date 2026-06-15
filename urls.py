2990200bd878afd57b76a5232c49e337cc01c39c
hmd1233 <hamadhumaid050@gmail.com>
2026-06-15 11:17:20 -0700

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+app_name = "onlinecourse"
+
+urlpatterns = [
+    path(
+        "submit/<int:course_id>/",
+        views.submit,
+        name="submit"
+    ),
+    path(
+        "show_exam_result/<int:submission_id>/",
+        views.show_exam_result,
+        name="show_exam_result"
+    ),
+]
